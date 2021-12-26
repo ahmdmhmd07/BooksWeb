fd7a4c41c0cbe1c6a17284064526c2e1e24d6470
ahmdmhmd07 <96378448+ahmdmhmd07@users.noreply.github.com>
2021-12-26 21:04:38 +0200

    BooksWeb

---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿SET IDENTITY_INSERT [dbo].[Categories] ON
+INSERT INTO [dbo].[Categories]([Id],[Name],[IsActive]) VALUES (1,'medicin',1)
+INSERT INTO [dbo].[Categories]([Id],[Name],[IsActive]) VALUES (2,'ektsad',1)
+INSERT INTO [dbo].[Categories]([Id],[Name],[IsActive]) VALUES (3,'tarekh',1)
+INSERT INTO [dbo].[Categories]([Id],[Name],[IsActive]) VALUES (4,'akl',1)
+INSERT INTO [dbo].[Categories]([Id],[Name],[IsActive]) VALUES (5,'science',1)
+INSERT INTO [dbo].[Categories]([Id],[Name],[IsActive]) VALUES (6,'art',1)
+INSERT INTO [dbo].[Categories]([Id],[Name],[IsActive]) VALUES (7,'math',1)
+INSERT INTO [dbo].[Categories]([Id],[Name],[IsActive]) VALUES (8,'sport',1)
+INSERT INTO [dbo].[Categories]([Id],[Name],[IsActive]) VALUES (9,'parmaga',1)
+INSERT INTO [dbo].[Categories]([Id],[Name],[IsActive]) VALUES (10,'adab',1)
+SET IDENTITY_INSERT [dbo].[Categories] OFF
